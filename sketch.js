ad183ee169989e4185e6be7166d43ab117da8c64
swati135 <34855416+swati135@users.noreply.github.com>
2020-04-29 19:02:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,14 +10,27 @@ function setup() {
 
   movingRect.debug = true;
   fixedRect.debug = true;
+  
+   gameobject1 = createSprite(100,200,50,80);
+  gameobject1.shapeColor= "green";
+
+  gameobject2 = createSprite(100,300,50,80);
+  gameobject2.shapeColor= "green";
+
+  gameobject3 = createSprite(300,200,50,80);
+  gameobject3.shapeColor= "green";
+
+  gameobject4 = createSprite(300,300,50,80);
+  gameobject4.shapeColor= "green";
+
 
 }
 
 function draw() {
   background(0);  
   
-  movingRect.x = mouseX;
-  movingRect.y = mouseY;
+  gameobject1.x = mouseX;
+  gameobject2.y = mouseY;
 
   if(movingRect.x - fixedRect.x <= movingRect.width/2+ fixedRect.width/2 &&
       fixedRect.x -movingRect.x <= movingRect.width/2+fixedRect.width/2
